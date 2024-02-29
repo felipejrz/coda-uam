86f77d08599675b2d1ddc26bca9fb6cdff7041d7
fredgolas <fredy00roca@gmail.com>
2024-02-29 18:57:32 +0200

    Add enviroment variables to container Development only, change variables when deploying on prod. Also removed the import of them in settings.py

--- coda-src/ssocial/settings.py
+++ coda-src/ssocial/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 # settings.py
 
 # TODO mochar esto en prod
-from .env_vars import *
+# from .env_vars import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
